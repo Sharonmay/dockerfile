ec2d337e35c3a800ecc50219e57442351eca5d6c
Sharon <qiantingmay@gmail.com>
2016-07-22 20:09:08 +0800

    add console.log

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require("express")
 const app = express()
 
-
 app.get("/", (req, res) => {
   res.json({name: "Guten", action: "kiss Sharon"})
 })
 
-
 app.listen(80)
+console.log("Express server listening on port %d in %s mode", 80, app.settings.env);
 
